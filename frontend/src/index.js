2c56a48714527417677e580fd5d49763c3a348f5
unknown <poojithagawni@gmail.com>
2025-04-16 23:43:29 +0530

    Re-added frontend folder to the main repo

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'animate.css'; // optional if you want animate.css classes
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
